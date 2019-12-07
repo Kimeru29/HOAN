140b3f62882ec583afa8627882d4211e573b76fb
Kimeru <pablo_p_29@outlook.com>
2019-12-06 23:04:25 -0600

    Detalles esteticos

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -25,7 +25,7 @@ export class AppComponent implements OnInit {
       // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
       offset: 120, // offset (in px) from the original trigger point
       delay: 200, // values from 0 to 3000, with step 50ms
-      duration: 850, // values from 0 to 3000, with step 50ms
+      duration: 950, // values from 0 to 3000, with step 50ms
       easing: 'ease', // default easing for AOS animations
       once: false, // whether animation should happen only once - while scrolling down
       mirror: false, // whether elements should animate out while scrolling past them
